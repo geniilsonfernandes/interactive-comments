da7476ea07d0eb52640bf2d5c2d015e8e4dbe612
Genilson <geniilsonfernandes@gmail.com>
2022-08-17 08:28:10 -0300

    :zap:feat(comments):add function for edit comentary

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,36 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import p from "prop-types";
 import { BsFillReplyFill as Reply, BsTrash as Trash } from "react-icons/bs";
 import { BiEditAlt as Edit } from "react-icons/bi";
 
 import * as S from "./styles";
+import Counter from "../Counter";
+import { useRef } from "react";
 
-const Comment = () => {
+const Comment = ({ hasAthor }) => {
+  const commnetEl = useRef();
+  const [editable, setEditable] = useState(false);
+
+  const handleClickToUpdate = (type) => {
+    if (type === "edit") {
+      setEditable(true);
+      commnetEl.current.focus();
+    } else {
+      setEditable(false);
+    }
+  };
   return (
     <S.Wrapper>
-      <S.Head>
-        <S.User>
-          <span>juliusomo</span>
-        </S.User>
-        <S.Date>1 month ago</S.Date>
-        <S.ButtonsGroup>
-          <S.EditButton>
-            <Edit color="#555ABA" /> Edit
-          </S.EditButton>
-          <S.DeleteButton>
-            <Trash color="#EE6764" /> Reply
-          </S.DeleteButton>
-          <S.ReplyButton>
-            <Reply color="#555ABA" /> Reply
-          </S.ReplyButton>
-        </S.ButtonsGroup>
-      </S.Head>
-      <S.Comment>
-        Impressive! Though it seems the drag feature could be improved. But
-        overall it looks incredible. You ve nailed the design and the
-        responsiveness at various breakpoints works really well.
-      </S.Comment>
+      <S.Counter>
+        <Counter />
+      </S.Counter>
+      <S.Content>
+        <S.Head>
+          <S.User>
+            <span>juliusomo</span>
+            {hasAthor && <S.UserTag>You</S.UserTag>}
+          </S.User>
+          <S.Date>1 month ago</S.Date>
+          <S.ButtonsGroup>
+            {hasAthor ? (
+              <>
+                <S.EditButton onClick={() => handleClickToUpdate("edit")}>
+                  <Edit color="#555ABA" /> Edit
+                </S.EditButton>
+                <S.DeleteButton>
+                  <Trash color="#EE6764" /> Delete
+                </S.DeleteButton>
+              </>
+            ) : (
+              <S.ReplyButton>
+                <Reply color="#555ABA" /> Reply
+              </S.ReplyButton>
+            )}
+          </S.ButtonsGroup>
+        </S.Head>
+        <S.Comment
+          contentEditable={editable}
+          isEditable={editable}
+          ref={commnetEl}
+          tabIndex={-1}
+        >
+          Impressive! Though it seems the drag feature could be improved. But
+          overall it looks incredible. You ve nailed the design and the
+          responsiveness at various breakpoints works really well.
+        </S.Comment>
+        {editable && (
+          <S.UpdateButton onClick={() => handleClickToUpdate("update")}>
+            Update
+          </S.UpdateButton>
+        )}
+      </S.Content>
     </S.Wrapper>
   );
+};
+
+Comment.propTypes = {
+  hasAthor: p.bool
 };
 
 export default Comment;
--- src/components/Comment/styles.js
+++ src/components/Comment/styles.js
@@ -1,16 +1,27 @@
 import styled, { css } from "styled-components";
+import { WrapperButtonWithIcon } from "../Styles";
+
+const wrapperSpacing = 16;
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     padding: 16px;
     background: ${theme.colors.bg[900]};
+    display: flex;
+    gap: ${wrapperSpacing / 2}px;
+    border-radius: 16px;
   `}
+`;
+
+export const Counter = styled.div``;
+export const Content = styled.div`
+  width: 100%;
 `;
 
 export const Head = styled.div`
   display: flex;
   align-items: center;
-
+  padding: 0 ${wrapperSpacing / 2}px;
   gap: 8px;
 `;
 
@@ -19,8 +30,24 @@ export const User = styled.div`
     font-weight: 600;
     font-size: 16px;
     color: ${theme.colors.text[100]};
+    display: flex;
+    align-items: center;
+    gap: 4px;
   `}
 `;
+
+export const UserTag = styled.span`
+  ${({ theme }) => css`
+    display: block;
+    background: ${theme.colors.secundary[100]};
+    padding: 2px 4px;
+    font-weight: 400;
+    font-size: 14px;
+    border-radius: 2px;
+    color: #fff;
+  `}
+`;
+
 export const Date = styled.span`
   ${({ theme }) => css`
     font-weight: 400;
@@ -35,36 +62,45 @@ export const ButtonsGroup = styled.div`
   gap: 8px;
 `;
 
-const WrapperButton = css`
-  display: flex;
-  gap: 4px;
-  padding: 4px;
-  border: none;
-  background: transparent;
-  font-weight: 600;
-  cursor: pointer;
-`;
-
 export const EditButton = styled.button`
   color: #555aba;
-  ${WrapperButton}
+  ${WrapperButtonWithIcon}
 `;
 export const DeleteButton = styled.button`
   color: #ee6764;
-  ${WrapperButton}
+  ${WrapperButtonWithIcon}
 `;
 
 export const ReplyButton = styled.button`
   color: #555aba;
-  ${WrapperButton}
+  ${WrapperButtonWithIcon}
+`;
+export const UpdateButton = styled.button`
+  margin-top: 8px;
+  color: #555aba;
+  margin-left: auto;
+  ${WrapperButtonWithIcon}
 `;
 
+const CommentModifiers = {
+  editable: (theme) => css`
+    border: 1px solid ${theme.colors.secundary[100]};
+    outline: 1px solid ${theme.colors.secundary[100]};
+    :focus {
+      border: 1px solid ${theme.colors.secundary[100]};
+      outline: 1px solid ${theme.colors.secundary[100]};
+    }
+  `
+};
 export const Comment = styled.p`
-  ${({ theme }) => css`
-    padding-top: 16px;
+  ${({ theme, isEditable }) => css`
+    margin-top: 8px;
+    padding: ${wrapperSpacing / 2}px;
     line-height: 24px;
     font-weight: 400;
     font-size: 16px;
     color: ${theme.colors.text[500]};
+    border-radius: 4px;
+    ${isEditable && CommentModifiers.editable(theme)}
   `}
 `;
