b1ca148431deb28f4bf38d9b6cc8148743f137b5
genilson <geniilsonfernandes@gmail.com>
2022-08-22 21:08:27 -0300

    :zap:chore(styles): create WrapperButton

--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -16,3 +16,22 @@ export const WrapperButtonWithIcon = css`
     }
   `}
 `;
+
+export const WrapperButton = css`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    gap: 4px;
+    padding: 8px 16px;
+    border: none;
+    background: ${theme.colors.secundary[100]};
+    font-weight: 400;
+    cursor: pointer;
+    border-radius: 8px;
+    transition: background ease-in-out 200ms;
+    color: #fff;
+    :hover {
+      background: ${theme.colors.secundary[200]};
+    }
+  `}
+`;
