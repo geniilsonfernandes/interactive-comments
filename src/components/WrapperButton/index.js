2b4deca8c0a94ce0304609a0fe55647c5f85ceea
Genilson <geniilsonfernandes@gmail.com>
2022-08-28 23:41:14 -0300

    ⚡️chore(WrapperButton):create WrapperButton

---
+++ src/components/WrapperButton/index.js
@@ -0,0 +1,31 @@
+import styled, { css } from "styled-components";
+
+const WrapperButtonModifier = {
+  isDisabled: () => css`
+    opacity: 0.5;
+    pointer-events: none;
+  `
+};
+export const WrapperButton = styled.button`
+  ${({ theme, isDisabled }) => css`
+    display: flex;
+    justify-content: center;
+    gap: 4px;
+    padding: 8px 16px;
+    border: none;
+    background: ${theme.colors.secundary[100]};
+    font-weight: 400;
+    cursor: pointer;
+    border-radius: 8px;
+    transition: background ease-in-out 200ms;
+    color: #fff;
+    font-size: 16px;
+    :hover {
+      background: ${theme.colors.secundary[200]};
+    }
+    :active {
+      transform: scale(0.96);
+    }
+    ${!!isDisabled && WrapperButtonModifier.isDisabled()}
+  `}
+`;
--- src/components/WrapperComment/index.jsx
+++ src/components/WrapperComment/index.jsx
@@ -5,15 +5,24 @@ import p from "prop-types";
 
 import * as S from "./styles";
 
-const WrapperComment = ({ content, replys, id, onSetReply }) => {
-  const replysComment = replys.filter((item) => item.reply_parent == id);
+const WrapperComment = ({ content, replys, id, onSetReply, user, userID }) => {
+  const getRepliesComment = replys.filter((item) => item.reply_parent == id);
 
   return (
     <S.Wrapper>
-      <Comment content={content} id={id} onReplySubmit={onSetReply} />
-      <S.WrapperReply>
-        {replysComment.map((comment) => (
+      <Comment
+        content={content}
+        id={id}
+        onReplySubmit={onSetReply}
+        user={user}
+        userID={userID}
+      />
+      <S.WrapperReply aria-label="replies">
+        {getRepliesComment.map((comment) => (
           <Comment
+            userID={userID}
+            user={user}
+            isReply={true}
             key={comment.comments_id}
             content={comment.comment}
             id={id}
@@ -27,7 +36,9 @@ const WrapperComment = ({ content, replys, id, onSetReply }) => {
 
 WrapperComment.propTypes = {
   content: p.string,
+  user: p.string,
   id: p.number,
+  userID: p.number,
   replys: p.array,
   onSetReply: p.func
 };
