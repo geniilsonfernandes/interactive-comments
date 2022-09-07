a9ba119ff7aed603aadbb66e70cf990df4263b91
Genilson <geniilsonfernandes@gmail.com>
2022-09-07 11:53:02 -0300

    refactor(comment): refactor component functions and props

--- src/components/WrapperComment/index.jsx
+++ src/components/WrapperComment/index.jsx
@@ -1,32 +1,50 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import Comment from "../Comment";
 import p from "prop-types";
 
 import * as S from "./styles";
+import { useStorageContext } from "../../context/dataContext";
 
-const WrapperComment = ({ content, replys, id, onSetReply, user, userID }) => {
-  const getRepliesComment = replys.filter((item) => item.reply_parent == id);
+const WrapperComment = ({
+  content,
+  replys,
+  comment_id,
+  onSetReply,
+  user,
+  userUid
+}) => {
+  const { localUid, localUser } = useStorageContext();
+
+  const getRepliesComment = replys.filter(
+    (item) => item.reply_parent == comment_id
+  );
+
+  const handleSubmitReply = (reply) => {
+    onSetReply &&
+      onSetReply({
+        user_name: localUser,
+        user_uid: localUid,
+        comment_id: Math.floor(Math.random() * 1000),
+        comment: reply.HTML,
+        text: reply.text,
+        replys_ids: [],
+        reply_parent: comment_id,
+        is_reply: true
+      });
+  };
 
   return (
     <S.Wrapper>
-      <Comment
-        content={content}
-        id={id}
-        onReplySubmit={onSetReply}
-        user={user}
-        userID={userID}
-      />
+      <Comment content={content} userName={user} userUid={userUid} />
       <S.WrapperReply aria-label="replies">
         {getRepliesComment.map((comment) => (
           <Comment
-            userID={userID}
-            user={user}
+            userUid={comment.user_uid}
+            userName={comment.user_name}
             isReply={true}
             key={comment.comments_id}
             content={comment.comment}
-            id={id}
-            onReplySubmit={onSetReply}
+            onSubmitReply={handleSubmitReply}
           />
         ))}
       </S.WrapperReply>
@@ -35,10 +53,10 @@ const WrapperComment = ({ content, replys, id, onSetReply, user, userID }) => {
 };
 
 WrapperComment.propTypes = {
+  comment_id: p.number,
   content: p.string,
   user: p.string,
-  id: p.number,
-  userID: p.number,
+  userUid: p.number,
   replys: p.array,
   onSetReply: p.func
 };
--- src/components/WrapperComment/styles.js
+++ src/components/WrapperComment/styles.js
@@ -1,8 +1,8 @@
 import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto;
+  display: flex;
+  flex-direction: column;
   gap: 16px;
 `;
 export const WrapperReply = styled.div`
@@ -12,13 +12,13 @@ export const WrapperReply = styled.div`
     gap: 16px;
     padding-left: 48px;
     position: relative;
+
     ::after {
       content: "";
       position: absolute;
       width: 2px;
       height: 100%;
       left: 24px;
-
       background: ${theme.colors.secundary[100]};
       opacity: 0.2;
     }
